ece9a402cce54fa5c9cd04c262b524809a1d7814
Tiago A. Fontenele <tiagoarrfon@gmail.com>
2022-11-02 17:39:11 -0300

    add title component

---
+++ src/app/custom-title/custom-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-title',
+  templateUrl: './custom-title.component.html',
+  styleUrls: ['./custom-title.component.css']
+})
+export class CustomTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
